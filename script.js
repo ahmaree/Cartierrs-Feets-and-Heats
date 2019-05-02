223eb151508ca2b10b71e743e8fc314195e6041b
ahmaree <43444323+ahmaree@users.noreply.github.com>
2019-05-02 09:50:03 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,8 @@
+$(".button1").click(function(){  
+   var name = $("#product").val();
+    
+    $("#").text("Hello, " + name+ "!"); 
+}); 
+ 
+var sneakers=["Jordans","Adiddas","Vans","New Balance","Converse"]; 
+var prices=[190,300,65,175,150]; 
